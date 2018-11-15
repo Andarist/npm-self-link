b4e1139ffd04275281440843ec1e66dd2a74f0d8
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-11-15 20:00:45 +0100

    Working tool

---
+++ README.md
@@ -0,0 +1,3 @@
+# npm-self-link
+
+A CLI tool which links the package in which it is run (if it's not already linked).
---
+++ bin/npm-self-link.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+'use strict'
+
+const npmSelfLink = require('..')
+
+npmSelfLink().then(
+  ({ pkgName }) => {
+    console.log(`Successfully made a self npm link to: ${pkgName}.`)
+  },
+  err => {
+    console.error(err)
+    process.exit(1)
+  },
+)
